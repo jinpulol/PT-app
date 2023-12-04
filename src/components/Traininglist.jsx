331cd8d7c1ebfcf26ec4c776d725f90536d6e825
jinpulol <jesselehmonen@gmail.com>
2023-12-04 18:32:53 +0200

    configs for publish

--- src/components/Traininglist.jsx
+++ src/components/Traininglist.jsx
@@ -86,8 +86,7 @@ function Traininglist() {
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', height: '80vh', margin: 0, padding: 0 }}>
-                <div className="ag-theme-material"
-                    style={{ height: 600, width: 1500 }}>
+                <div className="ag-theme-material" style={{ height: 600, width: 1500 }}>
                     <div style={{ display: 'flex', justifyContent: 'flex-start', padding: '10px' }}>
                         <AddTraining addTraining={addTraining} />
                     </div>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PT-app/',
   plugins: [react()],
-})
+  test: {
+  globals: true,
+  environment: 'jsdom',
+  },
+  })
+  
